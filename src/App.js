9022218fb3ca82e10e06d24db0e32d74ab67e5e2
Glen112 <escobar.glen@gmail.com>
2022-09-29 13:57:35 -0300

    anidadas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
+  BrowserRouter as Router
 } from "react-router-dom";
 import { Header, Body } from './components'
-
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
 
 export default function App() {
   return (
     <Router>
       <div>
         <Header/>
-        <hr />
         <Body/>
       </div>
     </Router>
