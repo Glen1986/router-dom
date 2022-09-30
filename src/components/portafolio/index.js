fb70d187d946e1593ad7bca47460db209e654131
Glen112 <escobar.glen@gmail.com>
2022-09-30 16:50:56 -0300

    RutasVariables

--- src/components/portafolio/index.js
+++ src/components/portafolio/index.js
@@ -1,28 +1,25 @@
-import { Link, Switch, Route } from 'react-router-dom'
+import { Link, useRouteMatch } from 'react-router-dom'
+import { Proyectos } from '../../components'
 import React from 'react';
 
-export default function(){
+export default ()=>{
+  const match = useRouteMatch()
+  const myUrl = match.url
+//
   return(
     <>
       <h1>Portafolio</h1>
       <ul>
         <li>
-          <Link to={'/portafolio/proyecto-1'}>Proyecto 1</Link>
+          <Link to={`${myUrl}/proyecto-1`}>Proyecto 1</Link>
         </li>
         <li>
-          <Link to={'/portafolio/proyecto-2'}>Proyecto 2</Link>
+          <Link to={`${myUrl}/proyecto-2`}>Proyecto 2</Link>
         </li>
       </ul>
       <div>
-        <Switch>
-          <Route path={'/portafolio/proyecto-1/'}>
-            <h2>Proyecto-1</h2>
-          </Route>
-          <Route path={'/portafolio/proyecto-2/'}>
-            <h2>Proyecto-2</h2>
-          </Route>
-        </Switch>
-      </div>
+        <Proyectos {...myUrl}/>
+     </div>
     </>
   )
 }
